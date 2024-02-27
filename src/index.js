b5cccdbfd866c46dc41fd0709635ab354b6cd7ed
Benjamin <Officialbenragland@gmail.com>
2024-02-26 22:03:45 -0500

    solution to tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const form = document.querySelector("form")
+
+
+  const renderElement = description => {
+    const todoList = document.getElementById("tasks")
+
+    const descDiv = document.createElement("li")
+    const desc = document.createElement("h2")
+    const deleteBtn = document.createElement("button")
+
+    desc.textContent = description
+    deleteBtn.textContent = "[x]"
+    
+    todoList.append(descDiv)
+    descDiv.appendChild(desc)
+    descDiv.appendChild(deleteBtn)
+
+    deleteBtn.addEventListener("click", () => {
+      descDiv.remove()
+    })
+
+  }
+
+  form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const input = e.target['new-task-description'].value
+    
+    renderElement(input)
+
+
+
+  })
+  
+  
+
 });
